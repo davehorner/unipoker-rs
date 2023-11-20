1ef3fd6ef7be35b7b9164af2e6145b91071eb302
Dave Horner <sites@tecdev.com>
2023-11-20 00:16:04 -0500

    error[E0597]: `hand_string` does not live long enough    --> src\main.rs:203:58        |        140 |         let hand_string = self.to_string();            |             ----------- binding `hand_string` declared here 	   ... 	   203 | 	       CardDeck::card_rank(&hand_string[4..6]); 	           | 		   ---------------------^^^^^^^^^^^------- 		       |                                     | 		       | 		           |                                     | 			   borrowed value does not live long enough 			       | 			       argument requires that `hand_string` is 			       borrowed for `'static` 			       ... 			       244 |     } 			           |     - `hand_string` dropped here 				   while still borrowed
    GPT 3.5

---
+++ src/main.rs
@@ -0,0 +1,342 @@
+use rand::seq::SliceRandom;
+use std::fmt;
+use regex::Regex;
+//use regex::bytes::Regex;
+
+// Define regex instances outside the method with static lifetime
+lazy_static::lazy_static! {
+    static ref HAND_FLUSH_REGEX: Regex = Regex::new(r"([\u{1f0a1}-\u{1f0ae}]{5})|([\u{1f0b1}-\u{1f0be}]{5})|([\u{1f0c1}-\u{1f0ce}]{5})|([\u{1f0d1}-\u{1f0de}]{5})").unwrap();
+    static ref HAND_STRAIGHT_REGEX: Regex = Regex::new(r"([\u{1f0a1}\u{1f0b1}\u{1f0d1}\u{1f0c1}][\u{1f0ae}\u{1f0be}\u{1f0de}\u{1f0ce}][\u{1f0ad}\u{1f0bd}\u{1f0dd}\u{1f0cd}][\u{1f0ab}\u{1f0bb}\u{1f0db}\u{1f0cb}][\u{1f0aa}\u{1f0ba}\u{1f0da}\u{1f0ca}])|[\u{1f0ae}\u{1f0be}\u{1f0de}\u{1f0ce}][\u{1f0ad}\u{1f0bd}\u{1f0dd}\u{1f0cd}][\u{1f0ab}\u{1f0bb}\u{1f0db}\u{1f0cb}][\u{1f0aa}\u{1f0ba}\u{1f0da}\u{1f0ca}][\u{1f0a9}\u{1f0b9}\u{1f0d9}\u{1f0c9}]|[\u{1f0ad}\u{1f0bd}\u{1f0dd}\u{1f0cd}][\u{1f0ab}\u{1f0bb}\u{1f0db}\u{1f0cb}][\u{1f0aa}\u{1f0ba}\u{1f0da}\u{1f0ca}][\u{1f0a9}\u{1f0b9}\u{1f0d9}\u{1f0c9}][\u{1f0a8}\u{1f0b8}\u{1f0d8}\u{1f0c8}]|[\u{1f0ab}\u{1f0bb}\u{1f0db}\u{1f0cb}][\u{1f0aa}\u{1f0ba}\u{1f0da}\u{1f0ca}][\u{1f0a9}\u{1f0b9}\u{1f0d9}\u{1f0c9}][\u{1f0a8}\u{1f0b8}\u{1f0d8}\u{1f0c8}][\u{1f0a7}\u{1f0b7}\u{1f0d7}\u{1f0c7}").unwrap();
+    static ref HAND_OF_A_KIND_REGEX: Regex = Regex::new(r"(\d)\1{3}").unwrap();
+}
+
+const NEW_DECK: [&str; 52] = [
+    // Spades
+    "🂡", "🂢", "🂣", "🂤", "🂥", "🂦", "🂧", "🂨", "🂩", "🂪", "🂫", "🂭", "🂮",
+    // Hearts
+    "🂱", "🂲", "🂳", "🂴", "🂵", "🂶", "🂷", "🂸", "🂹", "🂺", "🂻", "🂽", "🂾",
+    // Clubs
+    "🃑", "🃒", "🃓", "🃔", "🃕", "🃖", "🃗", "🃘", "🃙", "🃚", "🃛", "🃝", "🃞",
+    // Diamonds
+    "🃁", "🃂", "🃃", "🃄", "🃅", "🃆", "🃇", "🃈", "🃉", "🃊", "🃋", "🃍", "🃎",
+];
+const CARD_RANK_NAMES: [&str; 15] = [
+    "", "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "", "Queen", "King",
+];
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+struct CardDeck {
+    cards: Vec<&'static str>,
+}
+
+impl CardDeck {
+    fn new() -> Self {
+        let cards = NEW_DECK.to_vec();
+        Self { cards }
+    }
+
+    fn new_deck(&mut self) {
+        self.cards = NEW_DECK.to_vec();
+    }
+
+    fn shuffle(&mut self) {
+        self.new_deck();
+
+        let mut rng = rand::thread_rng();
+        self.cards.shuffle(&mut rng);
+    }
+
+    fn deal_one_card(&mut self) -> Option<&'static str> {
+        self.cards.pop()
+    }
+
+    fn card_rank(card: &'static str) -> usize {
+        let rank_of_card = card.chars().next().unwrap() as usize & 0xf;
+        if rank_of_card == 0x1 {
+            // Make Aces higher than Kings
+            0xf
+        } else {
+            rank_of_card
+        }
+    }
+
+    fn card_name(card: &'static str) -> &'static str {
+        let rank_of_card = Self::card_rank(card);
+        CARD_RANK_NAMES[rank_of_card]
+    }
+}
+
+
+
+
+struct Hand {
+    cards: Vec<&'static str>,
+    rank: usize,
+}
+
+impl Hand {    
+    const HAND_FLUSH_REGEX: &str = "([🂡-🂮]{5})|([🂱-🂾]{5})|([🃁-🃎]{5})|([🃑-🃞]{5})";
+    const HAND_STRAIGHT_REGEX: &str = "([🂡🂱🃁🃑][🂮🂾🃎🃞][🂭🂽🃋🃛][🂫🂻🃊🃚][🂪🂺🃉🃙])|\
+                                      [🂮🂾🃎🃞][🂭🂽🃋🃛][🂫🂻🃊🃚][🂪🂺🃉🃙][🂭🂽🃋🃛]|\
+                                      [🂫🂻🃊🃚][🂪🂺🃉🃙][🂭🂽🃋🃛][🂡-🂻🃁-🃛][🂠-🂮]|\
+                                      [🂭🂽🃋🃛][🂫🂻🃊🃚][🂭🂽🃋🃛][🂠-🂮][🂡-🂻]|\
+                                      [🂪🂺🃉🃙][🂫🂻🃊🃚][🂭🂽🃋🃛][🂡-🂻🃁-🃛][🂠-🂮]";
+    const HAND_OF_A_KIND_REGEX: &str = "(?:[🂡🂱🃁🃑]{2,4})|(?:[🂮🂾🃎🃞]{2,4})|(?:[🂭🂽🃋🃛]{2,4})|\
+                                       (?:[🂫🂻🃊🃚]{2,4})|(?:[🂪🂺🃉🃙]{2,4})|(?:[🂭🂽🃋🃛]{2,4})|\
+                                       (?:[🂡🂱🃁🃑🂮🂾🃎🃞]{3,4})|(?:[🂭🂽🃋🃛🂫🂻🃊🃚]{3,4})|\
+                                       (?:[🂪🂺🃉🃙🂭🂽🃋🃛]{3,4})|(?:[🂡🂱🃁🃑🂮🂾🃎🃞🂭🂽🃋🃛]{4})|\
+                                       (?:[🂫🂻🃊🃚🂭🂽🃋🃛]{4})|(?:[🂡🂱🃁🃑🂮🂾🃎🃞🂭🂽🃋🃛]{4})|(?:[🂪🂺🃉🃙🂡-🂻🃁-🃛]{4})|(?:[🂠-🂮]{5})";
+    const HAND_ROYAL_FLUSH: usize = 0x900000;
+    const HAND_STRAIGHT_FLUSH: usize = 0x800000;
+    const HAND_FOUR_OF_A_KIND: usize = 0x700000;
+    const HAND_FULL_HOUSE: usize = 0x600000;
+    const HAND_FLUSH: usize = 0x500000;
+    const HAND_STRAIGHT: usize = 0x400000;
+    const HAND_THREE_OF_A_KIND: usize = 0x300000;
+    const HAND_TWO_PAIR: usize = 0x200000;
+    const HAND_PAIR: usize = 0x100000;
+    fn new() -> Self {
+        let cards = Vec::new();
+        let rank = 0;
+        Self { cards, rank }
+    }
+
+    fn clear(&mut self) {
+        self.cards.clear();
+        self.rank = 0;
+    }
+
+    fn take_card(&mut self, card: &'static str) {
+        self.cards.push(card);
+    }
+
+    fn rank(&self) -> usize {
+        self.rank
+    }
+
+    fn to_string(&self) -> String {
+        self.cards.join("")
+    }
+
+    fn score(&mut self) {
+        // Sort highest rank to lowest
+        self.cards.sort_by(|a, b| {
+            CardDeck::card_rank(b).cmp(&CardDeck::card_rank(a))
+        });
+
+        /*
+        let hand_string: String = self.to_string();//self.cards.iter().copied().collect();
+
+        let flush_result = regex::Regex::new(Self::HAND_FLUSH_REGEX)
+            .unwrap()
+            .captures(&hand_string);
+        let straight_result = regex::Regex::new(Self::HAND_STRAIGHT_REGEX)
+            .unwrap()
+            .captures(&hand_string);
+        let of_a_kind_result = regex::Regex::new(Self::HAND_OF_A_KIND_REGEX)
+            .unwrap()
+            .captures(&hand_string);
+        */
+        let hand_string = self.to_string();
+        let flush_result = HAND_FLUSH_REGEX.captures(&hand_string);
+        let straight_result = HAND_STRAIGHT_REGEX.captures(&hand_string);
+        let of_a_kind_result = HAND_OF_A_KIND_REGEX.captures(&hand_string);
+
+        if let Some(flush_result) = flush_result {
+            if let Some(straight_result) = straight_result {
+                if straight_result.get(1).is_some() {
+                    self.rank = Hand::HAND_ROYAL_FLUSH;
+                } else {
+                    self.rank = Hand::HAND_STRAIGHT_FLUSH;
+                }
+            } else {
+                self.rank = Hand::HAND_FLUSH;
+            }
+
+            self.rank |=
+                CardDeck::card_rank(self.cards[0]) << 16 | CardDeck::card_rank(self.cards[1]) << 12;
+        } else if let Some(straight_result) = straight_result {
+            self.rank = Hand::HAND_STRAIGHT | CardDeck::card_rank(self.cards[0]) << 16
+                | CardDeck::card_rank(self.cards[1]) << 12;
+        } else if let Some(of_a_kind_result) = of_a_kind_result {
+            if of_a_kind_result[0].len() == 8 {
+                self.rank = Hand::HAND_FOUR_OF_A_KIND | CardDeck::card_rank(self.cards[0]);
+            } else {
+                let first_of_a_kind = &of_a_kind_result[0].to_string();
+                let remaining_cards_index =
+                    hand_string.find(first_of_a_kind).unwrap() + first_of_a_kind.len();
+                let mut second_of_a_kind_result = None;
+
+                if remaining_cards_index <= 6 {
+                    let remaining_cards = &hand_string[remaining_cards_index..];
+                    second_of_a_kind_result = regex::Regex::new(Self::HAND_OF_A_KIND_REGEX)
+                        .unwrap()
+                        .captures(remaining_cards);
+                }
+
+                if let Some(second_of_a_kind_result) = second_of_a_kind_result {
+                    if (first_of_a_kind.len() == 6 && second_of_a_kind_result[0].len() == 4)
+                        || (first_of_a_kind.len() == 4 && second_of_a_kind_result[0].len() == 6)
+                    {
+                        let three_of_a_kind_card_rank = CardDeck::card_rank(&first_of_a_kind[0..2]);
+                        let two_of_a_kind_card_rank = CardDeck::card_rank(&second_of_a_kind_result[0][0..2]);
+                        self.rank = Hand::HAND_FULL_HOUSE
+                            | (three_of_a_kind_card_rank << 16)
+                            | (three_of_a_kind_card_rank << 12)
+                            | (three_of_a_kind_card_rank << 8)
+                            | (two_of_a_kind_card_rank << 4)
+                            | two_of_a_kind_card_rank;
+                    } else if first_of_a_kind.len() == 4 && second_of_a_kind_result[0].len() == 4 {
+                        let first_pair_card_rank = CardDeck::card_rank(&first_of_a_kind[0..2]);
+                        let second_pair_card_rank =
+                            CardDeck::card_rank(&second_of_a_kind_result[0][0..2]);
+                        let other_card_rank;
+
+                        if first_of_a_kind.chars().next().unwrap() == hand_string.chars().next().unwrap() {
+                            if second_of_a_kind_result[0].chars().next().unwrap()
+                                == hand_string.chars().nth(4).unwrap()
+                            {
+                                other_card_rank =
+                                    CardDeck::card_rank(&hand_string[8..10]);
+                            } else {
+                                other_card_rank =
+                                    CardDeck::card_rank(&hand_string[4..6]);
+                            }
+                        } else {
+                            other_card_rank =
+                                CardDeck::card_rank(&hand_string[0..2]);
+                        }
+
+                        self.rank = Hand::HAND_TWO_PAIR
+                            | (first_pair_card_rank << 16)
+                            | (first_pair_card_rank << 12)
+                            | (second_pair_card_rank << 8)
+                            | (second_pair_card_rank << 4)
+                            | other_card_rank;
+                    }
+                } else {
+                    let of_a_kind_card_rank =
+                        CardDeck::card_rank(&first_of_a_kind[0..2]);
+                    let mut other_cards_rank = 0;
+
+                    for card in &self.cards {
+                        let card_rank = CardDeck::card_rank(card);
+                        if card_rank != of_a_kind_card_rank {
+                            other_cards_rank = (other_cards_rank << 4) | card_rank;
+                        }
+                    }
+
+                    if first_of_a_kind.len() == 6 {
+                        self.rank = Hand::HAND_THREE_OF_A_KIND
+                            | (of_a_kind_card_rank << 16)
+                            | (of_a_kind_card_rank << 12)
+                            | (of_a_kind_card_rank << 8)
+                            | other_cards_rank;
+                    } else {
+                        self.rank = Hand::HAND_PAIR
+                            | (of_a_kind_card_rank << 16)
+                            | (of_a_kind_card_rank << 12)
+                            | other_cards_rank;
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl fmt::Display for Hand {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.cards.join(""))
+    }
+}
+
+struct Player {
+    name: String,
+    wins: usize,
+    hand_type_counts: [usize; 10],
+    hand: Hand,
+}
+
+impl Player {
+    fn new(name: &str) -> Self {
+        let name = name.to_string();
+        let wins = 0;
+        let hand_type_counts = [0; 10];
+        let hand = Hand::new();
+        Self {
+            name,
+            wins,
+            hand_type_counts,
+            hand,
+        }
+    }
+
+    fn score_hand(&mut self) {
+        self.hand.score();
+        let hand_type = self.hand.rank >> 20;
+        self.hand_type_counts[hand_type as usize] += 1;
+    }
+
+    fn won_hand(&mut self) {
+        self.wins += 1;
+    }
+
+    fn name(&self) -> &str {
+        &self.name
+    }
+
+    fn hand(&self) -> String {
+        self.hand.to_string()
+    }
+}
+
+fn play_hands(players: &mut Vec<Player>) {
+    let mut card_deck = CardDeck::new();
+    let mut hands_played = 0;
+    let mut highest_rank = 0;
+
+    while hands_played < 2000 {
+        card_deck.shuffle();
+
+        for player in players.iter_mut() {
+            player.hand.clear();
+        }
+
+        for _ in 0..5 {
+            for player in players.iter_mut() {
+                if let Some(card) = card_deck.deal_one_card() {
+                    player.hand.take_card(card);
+                }
+            }
+        }
+
+        for player in players.iter_mut() {
+            player.score_hand();
+        }
+
+        hands_played += 1;
+
+        highest_rank = players
+            .iter()
+            .map(|player| player.hand.rank)
+            .max()
+            .unwrap_or_default();
+
+        for player in players.iter_mut() {
+            if player.hand.rank == highest_rank {
+                player.won_hand();
+            }
+        }
+    }
+}
+
+fn main() {
+    // Create and initialize players
+    let mut players = vec![Player::new("Player1"), Player::new("Player2")];
+
+    // Play hands
+    play_hands(&mut players);
+
+    // Display results or perform further actions
+}
+
